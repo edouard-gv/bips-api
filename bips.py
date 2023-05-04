de265b64191c84eba8cdf5c096725fd281db8992
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2023-05-04 16:26:51 +0200

    Première version de l'API

---
+++ bips.py
@@ -0,0 +1,60 @@
+import os
+import json
+from datetime import datetime, timedelta
+import uuid
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+# Configuration de la base de données DynamoDB
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("Bips")
+
+# Fonction pour insérer un nouveau Bip dans la base de données
+def add_bip(data):
+    bip = {
+        "id": str(uuid.uuid4()),
+        "pseudo": data["pseudo"],
+        "status_code": data["status_code"],
+        "location": data["location"],
+        "latitude": data.get("latitude", None),
+        "longitude": data.get("longitude", None),
+        "timestamp": datetime.utcnow().isoformat(),
+    }
+    table.put_item(Item=bip)
+    return bip["id"]
+
+# Fonction pour récupérer les Bips qui ont été postés au même endroit aujourd'hui
+def get_bips(location):
+    start_time = datetime.utcnow().date().isoformat()
+    end_time = (datetime.utcnow().date() + timedelta(days=1)).isoformat()
+
+    response = table.query(
+        IndexName="location-timestamp-index",
+        KeyConditionExpression=Key("location").eq(location) & Key("timestamp").between(start_time, end_time),
+    )
+
+    return [
+        {"pseudo": bip["pseudo"], "status_code": bip["status_code"], "timestamp": bip["timestamp"]}
+        for bip in response["Items"]
+    ]
+
+# Fonction principale pour AWS Lambda
+def lambda_handler(event, context):
+    http_method = event["requestContext"]["http"]["method"]
+
+    if http_method == "POST":
+        data = json.loads(event["body"])
+        bip_id = add_bip(data)
+        response = {"message": f"Bip added with ID: {bip_id}"}
+    elif http_method == "GET":
+        location = event["queryStringParameters"]["location"]
+        response = get_bips(location)
+    else:
+        response = {"error": "Invalid request"}
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(response),
+    }
